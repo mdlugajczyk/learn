0594d36b4cc22343742974b797ab871666dfe9bd
Marcin Dlugajczyk <dlugajczykmarcin@gmail.com>
2026-08-16 21:34:27 +0200

    fix: refresh Czytaj shell cache

--- public/czytaj/sw.js
+++ public/czytaj/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'czytaj-shell-v2';
+const SHELL_CACHE = 'czytaj-shell-v3';
 const PACK_PREFIX = 'czytaj-pack-';
 const SCOPE_URL = self.registration.scope;
 const SCOPE_PATH = new URL(SCOPE_URL).pathname;
